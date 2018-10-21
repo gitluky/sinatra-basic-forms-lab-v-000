1eeb677117159b638621a2f5514c045c6c3f0945
Jonathan Luk <jluk87@gmail.com>
2018-10-21 01:58:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+  get '/' do
+    erb :display_puppy
+  end
+
 end
